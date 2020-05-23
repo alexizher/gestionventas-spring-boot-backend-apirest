62b44afe6b216babd3c6b8841e59cf7d2fc37abf
Alexis Herrera <alexizher@gmail.com>
2020-05-23 00:11:24 -0500

    Update GestionventasSpringBootBackendApirestApplicationTests.java
    se quita el comentario en test

--- src/test/java/com/gestionventas/springboot/backend/apirest/GestionventasSpringBootBackendApirestApplicationTests.java
+++ src/test/java/com/gestionventas/springboot/backend/apirest/GestionventasSpringBootBackendApirestApplicationTests.java
@@ -1,12 +1,13 @@
 package com.gestionventas.springboot.backend.apirest;
 
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GestionventasSpringBootBackendApirestApplicationTests {
 
-/*	@Test
+	@Test
 	void contextLoads() {
 	}
-*/
+
 }
