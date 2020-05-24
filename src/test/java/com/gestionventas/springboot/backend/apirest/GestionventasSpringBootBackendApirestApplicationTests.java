c03a98a80849b93796b3e2732c740f5a805fabe1
Alexis Herrera <alexizher@gmail.com>
2020-05-24 11:58:42 -0500

    Update GestionventasSpringBootBackendApirestApplicationTests.java
    comentar librería para despliegue heroku

--- src/test/java/com/gestionventas/springboot/backend/apirest/GestionventasSpringBootBackendApirestApplicationTests.java
+++ src/test/java/com/gestionventas/springboot/backend/apirest/GestionventasSpringBootBackendApirestApplicationTests.java
@@ -1,11 +1,14 @@
 package com.gestionventas.springboot.backend.apirest;
 
-import org.junit.Test;
+//librería para app en horuku
+//import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GestionventasSpringBootBackendApirestApplicationTests {
 
+	//comentar este método para despliegue en Heroku
 	@Test
 	void contextLoads() {
 	}
