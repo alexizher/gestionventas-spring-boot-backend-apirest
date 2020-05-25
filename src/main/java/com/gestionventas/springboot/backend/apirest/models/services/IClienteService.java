d68226d1575beb11e4f851d50c2fec98e1dac1fb
Alexis Herrera <alexizher@gmail.com>
2020-05-25 14:13:33 -0500

    implementación de métodos
    implementación de métodos para buscar por id, guardar y eliminar

--- src/main/java/com/gestionventas/springboot/backend/apirest/models/services/IClienteService.java
+++ src/main/java/com/gestionventas/springboot/backend/apirest/models/services/IClienteService.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import com.gestionventas.springboot.backend.apirest.models.entity.Cliente;
+import com.gestionventas.springboot.backend.apirest.models.entity.Factura;
 import com.gestionventas.springboot.backend.apirest.models.entity.Region;
 
 /**
@@ -14,18 +15,25 @@ import com.gestionventas.springboot.backend.apirest.models.entity.Region;
  * @author Miguel
  */
 
+// con esta clase administramos y manejamos los diferentes accesos a los Dao
 public interface IClienteService {
-    
-    public List<Cliente> findAll(); //retorna el cliente guardado
-    
-    public Page<Cliente> findAll(Pageable pageable); //Recibe el objeto paginable
-    
-    public Cliente save(Cliente cliente); //retorna el cliente guardado
-    
-    public Cliente findById(Long id); //retorna el cliente por id
 
-    public void delete(Long id); //elimina el cliente por id
-    
-    public List<Region> findAllRegiones();
-    
+	public List<Cliente> findAll(); // retorna el cliente guardado
+
+	public Page<Cliente> findAll(Pageable pageable); // Recibe el objeto paginable
+
+	public Cliente save(Cliente cliente); // retorna el cliente guardado
+
+	public Cliente findById(Long id); // retorna el cliente por id
+
+	public void delete(Long id); // elimina el cliente por id
+
+	public List<Region> findAllRegiones();
+
+	public Factura findFacturaById(Long id);
+
+	public Factura saveFactura(Factura factura);
+
+	public void deleteFacturaById(Long id);
+
 }
