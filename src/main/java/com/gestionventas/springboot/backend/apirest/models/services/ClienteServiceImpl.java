e3f5c600512a3e70143a9eb81ef5ffeeb288bc76
Alexis Herrera <alexizher@gmail.com>
2020-05-25 14:17:01 -0500

    Update ClienteServiceImpl.java
    Se agregan ,métodos de la interfaz con su respectivo @Transactional

--- src/main/java/com/gestionventas/springboot/backend/apirest/models/services/ClienteServiceImpl.java
+++ src/main/java/com/gestionventas/springboot/backend/apirest/models/services/ClienteServiceImpl.java
@@ -9,7 +9,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.gestionventas.springboot.backend.apirest.models.dao.IClienteDao;
+import com.gestionventas.springboot.backend.apirest.models.dao.IFacturaDao;
 import com.gestionventas.springboot.backend.apirest.models.entity.Cliente;
+import com.gestionventas.springboot.backend.apirest.models.entity.Factura;
 import com.gestionventas.springboot.backend.apirest.models.entity.Region;
 
 /**
@@ -19,17 +21,20 @@ import com.gestionventas.springboot.backend.apirest.models.entity.Region;
  */
 
 @Service
-public class ClienteServiceImpl implements IClienteService{
+public class ClienteServiceImpl implements IClienteService {
 
-    @Autowired //anotación para injectar el cliente Dao
-    private IClienteDao clienteDao;
-    
-    @Override
-    @Transactional(readOnly = true)
-    public List<Cliente> findAll() {
-        return (List<Cliente>) clienteDao.findAll();
-    }
-    
+	@Autowired // anotación para injectar el cliente Dao
+	private IClienteDao clienteDao;
+
+	@Autowired
+	private IFacturaDao facturaDao;
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Cliente> findAll() {
+		return (List<Cliente>) clienteDao.findAll();
+	}
+
 	@Override
 	@Transactional(readOnly = true)
 	public Page<Cliente> findAll(Pageable pageable) {
@@ -37,31 +42,48 @@ public class ClienteServiceImpl implements IClienteService{
 		return clienteDao.findAll(pageable);
 	}
 
-    @Override
-    @Transactional(readOnly = true)
-    public Cliente findById(Long id) {
-        return clienteDao.findById(id).orElse(null);
-    }
+	@Override
+	@Transactional(readOnly = true)
+	public Cliente findById(Long id) {
+		return clienteDao.findById(id).orElse(null);
+	}
 
-    @Override
-    @Transactional
-    public Cliente save(Cliente cliente) {
-        return clienteDao.save(cliente);
-    }
+	@Override
+	@Transactional
+	public Cliente save(Cliente cliente) {
+		return clienteDao.save(cliente);
+	}
 
-    @Override
-    @Transactional
-    public void delete(Long id) {
-    	clienteDao.deleteById(id);
-        
-    }
+	@Override
+	@Transactional
+	public void delete(Long id) {
+		clienteDao.deleteById(id);
+
+	}
 
 	@Override
 	@Transactional(readOnly = true)
 	public List<Region> findAllRegiones() {
-		
+
 		return clienteDao.findAllRegiones();
 	}
 
+	@Override
+	@Transactional(readOnly = true)
+	public Factura findFacturaById(Long id) {
+		return facturaDao.findById(id).orElse(null);
+	}
+
+	@Override
+	@Transactional
+	public Factura saveFactura(Factura factura) {
+		return facturaDao.save(factura);
+	}
+
+	@Override
+	@Transactional
+	public void deleteFacturaById(Long id) {
+		facturaDao.deleteById(id);
+	}
 
 }
