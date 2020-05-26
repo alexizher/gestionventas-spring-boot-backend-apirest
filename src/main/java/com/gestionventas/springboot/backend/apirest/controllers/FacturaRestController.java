64ef8b0861abcd0c78e4be63784f7ff8b75c8d28
Alexis Herrera <alexizher@gmail.com>
2020-05-26 18:00:52 -0500

    método del controlador para eliminar factura
    método del controlador para eliminar factura

--- src/main/java/com/gestionventas/springboot/backend/apirest/controllers/FacturaRestController.java
+++ src/main/java/com/gestionventas/springboot/backend/apirest/controllers/FacturaRestController.java
@@ -3,6 +3,7 @@ package com.gestionventas.springboot.backend.apirest.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +25,13 @@ public class FacturaRestController {
 	@ResponseStatus(HttpStatus.OK)
 	public Factura show(@PathVariable Long id) {
 		return clienteService.findFacturaById(id);
+	}
+	
+	//método del controlador para eliminar factura
+	@DeleteMapping("/facturas/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public  void delete(@PathVariable Long id) {
+		clienteService.deleteFacturaById(id);
 	}
 
 }
