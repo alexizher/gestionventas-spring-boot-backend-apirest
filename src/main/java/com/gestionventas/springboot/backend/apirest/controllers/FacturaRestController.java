af9a5a792cf387c65c46ec42b4821a137e1919ed
Alexis Herrera <alexizher@gmail.com>
2020-05-27 20:39:41 -0500

    corregir error en crear factura

--- src/main/java/com/gestionventas/springboot/backend/apirest/controllers/FacturaRestController.java
+++ src/main/java/com/gestionventas/springboot/backend/apirest/controllers/FacturaRestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -50,7 +51,7 @@ public class FacturaRestController {
 		return clienteService.findProductoByNombre(term);
 	}
 	
-	@GetMapping("/facturas")
+	@PostMapping("/facturas")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Factura crear(@RequestBody Factura factura) {
 		return clienteService.saveFactura(factura);
